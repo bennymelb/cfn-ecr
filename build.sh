a2a67457bd03558e9f31aadecbb56058025c67a9
Benny <bennymelb@gmail.com>
2020-12-10 00:59:22 +1100

    sanitizer the template

--- build.sh
+++ build.sh
@@ -4,17 +4,20 @@
 set -e 
 
 usage() { 
-    echo "Usage: $0 [-e <environment dev|test|prod>] [-r <aws region e.g. us-east-1>]" 1>&2
+    echo "Usage: $0 [-e <environment dev|test|prod>] [-r <aws region e.g. us-east-1>] [-a <the application this resource belongs to e.g. awesomeapp>]" 1>&2
     exit 1 
 }
 
-while getopts ":e:r:h:" o; do
+while getopts ":e:r:o:g:a:h:" o; do
     case "${o}" in
         e)
             env=${OPTARG}
             ;;
         r)
             region=${OPTARG}
+            ;;
+        a)
+            app=${OPTARG}
             ;;
         h)
             usage
@@ -26,13 +29,9 @@ while getopts ":e:r:h:" o; do
 done
 shift $((OPTIND-1))
 
-if [ -z "${env}" ] || [ -z "${region}" ] ; then
+if [ -z "${env}" ] || [ -z "${region}" ] || [ -z "${app}" ] ; then
     usage
 fi
-
-opco="cyt"
-group="host"
-app="gear"
 
 # Test if aws cli is installed
 r=$(aws --version)
@@ -45,29 +44,12 @@ fi
 export AWS_DEFAULT_REGION=${region}
 
 # Create ui ECR
-echo "Deploying ui ECR"
-aws cloudformation deploy --template-file cfn/ecr.yaml --stack-name "${env}-${app}-ui-ecr" --parameter-overrides \
+service="ui"
+echo "Deploying ${env} ${app} ${service} repo"
+aws cloudformation deploy --template-file cfn/ecr.yaml --stack-name "${env}-${app}-${service}-ecr" --parameter-overrides \
 Environment=${env} \
 AppName=${app} \
-Opco=${opco} \
-Group=${group} \
-Service=ui \
+Service=${service} \
 --tags \
-dhr-opco=${opco} \
-dhr-group=${group} \
-dhr-app=${app} \
-dhr-env=${env} \
-
-# Create engine ECR
-echo "Deploying engine ECR"
-aws cloudformation deploy --template-file cfn/ecr.yaml --stack-name "${env}-${app}-engine-ecr" --parameter-overrides \
-Environment=${env} \
-AppName=${app} \
-Opco=${opco} \
-Group=${group} \
-Service=engine \
---tags \
-dhr-opco=${opco} \
-dhr-group=${group} \
-dhr-app=${app} \
-dhr-env=${env} \
+app=${app} \
+env=${env} \
